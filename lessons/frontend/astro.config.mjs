078e05597e7e9afceba574d76d41dc6b3429ff5b
Local Epsilon User <localepsilon@cis0678>
2025-09-01 12:42:29 -0700

    astro.config.mjs added output/hybrid, adapter/node/standalone

--- lessons/frontend/astro.config.mjs
+++ lessons/frontend/astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: 'hybrid',
+  adapter: node ({
+    mode: 'standalone'
+  }),
   server: {
     proxy: {
       '/api': 'http://localhost:8080'
